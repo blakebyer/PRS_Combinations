e1d90e3dfcaf84a84f28a3a0a6b2c4bc4b738e1d
bbyer04 <143546151+bbyer04@users.noreply.github.com>
2024-05-06 17:48:13 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import numpy as np
 from scipy import stats as spy
 
 #Load Files
-adni_path = r"C:\Users\bbyer\Documents\UniversityofKentucky\MillerLab\PRSKB\ADNIMERGE_21Nov2023.csv" #ADNIMERGE is a CSV with Demographic Information About Cases
-tsv_path = r"C:\Users\bbyer\Documents\UniversityofKentucky\MillerLab\PRSKB\hady_ADNI_trial4.tsv" #TSV is the raw output file from the PRSKB CLI Tool
+adni_path = r"" #ADNIMERGE is a CSV with Demographic Information About Cases
+tsv_path = r"" #TSV is the raw output file from the PRSKB CLI Tool
 
 #Create Dataframes
 demographics = ['PTID','DX','DX_bl','PTGENDER','PTRACCAT','AGE','APOE4','EXAMDATE']
