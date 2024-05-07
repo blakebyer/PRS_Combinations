64733d8045a5a07e865c4d2d621a29dc26dc4aeb
bbyer04 <143546151+bbyer04@users.noreply.github.com>
2024-05-07 15:57:18 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='bbyer04',
+    version='1.0',
+    authors='',
+    url='https://github.com/jmillerlab/PRS_Combinations',
+    license='LICENSE.txt',
+    description='PRS Combinations',
+    install_requires=[
+        "numpy >= 1.14.2",
+        "scipy >= 1.0.0",
+        "pandas >= 1.3.3",
+        "matplotlib >= 
+    ],
+)
