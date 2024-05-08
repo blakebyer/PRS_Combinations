8637a4880933bd33efa059583cd9a3f5a57c3956
bbyer04 <143546151+bbyer04@users.noreply.github.com>
2024-05-08 11:17:07 -0400

    Update config.py

--- prscombos/config.py
+++ prscombos/config.py
@@ -1,14 +1,8 @@
-import pandas as pd
-import numpy as np
-from scipy import stats as spy
+from pathlib import Path
 
-#Load Files
-adni_path = r"" #ADNIMERGE is a CSV with Demographic Information About Cases
-tsv_path = r"" #TSV is the raw output file from the PRSKB CLI Tool
+tsv_dir = Path('/path/to/some/logical/parent/directory')
+tsv_path = tsv_dir / 'my_file.tsv'
 
-#Create Dataframes
-demographics = ['PTID','DX','DX_bl','PTGENDER','PTRACCAT','AGE','APOE4','EXAMDATE']
-tsv_df = pd.read_csv(tsv_path, header=0, usecols=['Sample','Study ID','Reported Trait','Percentile'],sep='\t')
-adni_df = pd.read_csv(adni_path, header=0,usecols=demographics)
-merged_df = pd.DataFrame()
-final_df = pd.DataFrame()
+csv_dir = Path('/path/to/some/logical/parent/dir')
+csv_path = csv_dir / 'my_file.csv'
+
