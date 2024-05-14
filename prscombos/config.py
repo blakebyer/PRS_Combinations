2006c48c7e0f3e7d56de92f767d80fbe065e06bd
bbyer04 <143546151+bbyer04@users.noreply.github.com>
2024-05-13 21:33:11 -0400

    Update config.py

--- prscombos/config.py
+++ prscombos/config.py
@@ -1,8 +1,14 @@
 from pathlib import Path
 
-tsv_dir = Path('/path/to/some/logical/parent/directory')
-tsv_path = tsv_dir / 'my_file.tsv'
+tsv_dir = Path(r'\logical\path\to\parent\directory') #PRSKB TSV Directory
+tsv_path = tsv_dir / 'myfile.tsv' # PRSKB File Name
+tsv_cols = ['Sample','Study ID','Reported Trait','Percentile'] # TSV Columns
 
-csv_dir = Path('/path/to/some/logical/parent/dir')
-csv_path = csv_dir / 'my_file.csv'
+adni_dir = Path(r'\logical\path\to\parent\directory') #ADNIMERGE Directory
+adni_path = adni_dir / 'adnimerge.csv' # adnimerge File Name
+adni_cols = ['RID','PTID','PTGENDER','PTRACCAT','AGE','APOE4'] # ADNI Demographics
+
+dx_dir = Path(r'\logical\path\to\parent\directory')
+dx_path = dx_dir / 'dxsum.csv' # dxsum File Name
+dx_cols = ['RID','DIAGNOSIS','EXAMDATE']
 
